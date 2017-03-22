a05699fe9cb112fd8da3903dd65d830abc8809bf
Joel Rorseth <joelrorseth@gmail.com>
2017-03-22 11:41:32 -0400

    Redesign algorithm to dynamically draw tree of any size

--- Two-Trees.playground/Sources/TreeScene.swift
+++ Two-Trees.playground/Sources/TreeScene.swift
@@ -40,10 +40,23 @@ public class TreeScene: SKScene {
         tree.insert(value: 10)
         tree.insert(value: 9)
         tree.insert(value: 1)
-        tree.insert(value: 11)
+        tree.insert(value: 11) // cutoff
+        //tree.insert(value: 13)
+        //tree.insert(value: 6)
+        //tree.insert(value: 3)
+        //tree.insert(value: 8)
+        
+        let topHalfView = CGSize(width: self.size.width, height: self.size.height / 2)
+        let treeHeight = tree.height()
+        print(treeHeight)
 
         // Testing: Draw binary tree created above
-        self.drawBST(tree: tree, point: CGPoint(x: 150, y: 300), offset: CGFloat(tree.height()))
+        //self.drawBST(tree: tree, point: CGPoint(x: 150, y: 300), offset: CGFloat(tree.height()))
+        self.drawBST(tree: tree,
+                     within: topHalfView,
+                     at: CGPoint(x: topHalfView.width / 2, y: self.size.height - 30),
+                     offset: CGFloat(tree.height()),
+                     originalHeight: treeHeight)
     }
     
     // =====================================
@@ -60,25 +73,34 @@ public class TreeScene: SKScene {
     
     // =====================================
     // =====================================
-    public func drawBST(tree: BinarySearchTree<Int>, point: CGPoint, offset: CGFloat) {
+    public func drawBST(tree: BinarySearchTree<Int>, within size: CGSize, at point: CGPoint, offset: CGFloat, originalHeight: Int) {
         
         // Use temp placeholders for size and color
-        let nodeRadius = CGFloat(18)
         let nodeColor = SKColor.white
         let temp = tree
+            
+        let nodeRadius = CGFloat(size.width / (2 * pow(2, CGFloat(originalHeight + 1))))
+        let spacingForDepth = CGFloat(size.width / (2 * pow(2, CGFloat(temp.depth() + 1))))
+        
+        
+        
         
         // Traverse left branch, decrement x and increment y positions
         if (temp.hasLeftChild) {
             drawBST(tree: temp.left!,
-                    point: CGPoint(x: point.x - 30 - (30*offset), y: point.y - 60),
-                    offset: (offset - 1))
+                    within: size,
+                    at: CGPoint(x: point.x - spacingForDepth, y: point.y - 60),
+                    offset: (offset - 1),
+                    originalHeight: originalHeight)
         }
         
         // Traverse right branch, increment x and y positions
         if (temp.hasRightChild) {
             drawBST(tree: temp.right!,
-                    point: CGPoint(x: point.x + 30 + (30*offset), y: point.y - 60),
-                    offset: (offset - 1))
+                    within: size,
+                    at: CGPoint(x: point.x + spacingForDepth, y: point.y - 60),
+                    offset: (offset - 1),
+                    originalHeight: originalHeight)
         }
         
         // Add node now that traversal has exhausted itself to this Node
@@ -96,6 +118,7 @@ public class TreeScene: SKScene {
             let touchedNodes = self.nodes(at: location)
             
             if (touchedNodes.isEmpty) { return }
+            print("Touched x:\(location.x) y:\(location.y)")
             
             let parentNode = touchedNodes[touchedNodes.count - 1]
             if parentNode is Node {
