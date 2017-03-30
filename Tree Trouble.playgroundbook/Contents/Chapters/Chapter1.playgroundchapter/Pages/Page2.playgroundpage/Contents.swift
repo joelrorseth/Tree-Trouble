4dddebaf6bcb29dcd80409b9d5d654e071912590
Joel Rorseth <joelrorseth@gmail.com>
2017-03-29 22:27:12 -0400

    Fix manifest files for playground chapters and add more text

--- Tree Trouble.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/Contents.swift
+++ Tree Trouble.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/Contents.swift
@@ -2,7 +2,7 @@
 //: The most important part of the Binary Search Tree is the ordering property. For every node, the value of each left child node is *less than* the value of itself, and the value of each right child node is *greater than* itself! Easy!
 
 //: ## Insertion
-//: To insert a node, start at the root (the topmost node). Determine if it is less than or greater than, and follow the left branch or right branch respectively. If there are no more branches, leave it!
+//: To insert a node, start at the root (the topmost node). Determine if the value of the new node is less than or greater than this node, and follow the left edge or right edge respectively. If there is no edge in this direction, create a *new edge* here and connect it to the new node!
 
 //: ## Can You Find the Missing Piece?
 //: Drag the nodes at the bottom into the correct place!
--- Tree Trouble.playgroundbook/Contents/Chapters/Chapter2.playgroundchapter/Pages/Page3.playgroundpage/Contents.swift
+++ Tree Trouble.playgroundbook/Contents/Chapters/Chapter2.playgroundchapter/Pages/Page3.playgroundpage/Contents.swift
@@ -1,9 +1,11 @@
 //: ## Traversal
-//: One of the features of binary search trees is the fact that it is always sorted. To extract the nodes in ascending sorted order we use an inorder traversal!
+//: One of the most notable features to the Binary Search Tree is the natural sorted nature of its elements. Using an [in-order traversal](https://en.wikipedia.org/wiki/Tree_traversal#In-order), a Binary Search Tree of any size can output its elements in ascending, sorted order!
 
 //: ## In-Order Traversal
-//: In a traversal, we want to retrieve all nodes in a certain order. From the root, We will 'visit' all nodes beginning with the left, then root, then right node. If any node has a child, we advance to that node and repeat the same process before moving on to the other branches.
+//: In a [tree traversal](https://en.wikipedia.org/wiki/Tree_traversal), we simply retrieve or process all tree nodes in a given order. To perform an *in-order traversal*, we will start at the root node, checking if it exists. The algorithm will then traverse the left subtree by recursively calling the traversal function again. It will then output the value of the current root node, followed by traversing the right subtree recursively.
+
+//:
 
 import PlaygroundSupport
 
-//: [Next](@next)
+//: [Challenge](@next)
